cc7cc81e874d58911815294c271eef77b3157fe3
rothblaa <adam.j.rothblatt@outlook.com>
2024-10-08 22:27:37 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { EmployeeTableComponent } from './employee-table/employee-table.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmployeeTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/employee-table/employee-table.component.ts
@@ -0,0 +1,25 @@
+import { EmployeeService } from './../employee.service';
+import { Employee } from './../../models/employee';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'employee-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './employee-table.component.html',
+  styleUrl: './employee-table.component.css'
+})
+export class EmployeeTableComponent {
+
+  employees: Employee[] = [];
+  
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(){
+    this.employeeService.getEmployees().subscribe((data: Employee[]) => {
+      this.employees = data;
+      console.log(data);
+    })
+  }
+
+}
