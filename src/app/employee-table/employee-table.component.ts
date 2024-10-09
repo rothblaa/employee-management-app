6a6adedcbaa7d1a266103f1fd6d1a0f1c88b961e
rothblaa <adam.j.rothblatt@outlook.com>
2024-10-09 09:52:23 -0500

    imports to table.ts and creation of table html, ng g c employee-form

--- src/app/employee-table/employee-table.component.ts
+++ src/app/employee-table/employee-table.component.ts
@@ -1,18 +1,19 @@
 import { EmployeeService } from './../employee.service';
 import { Employee } from './../../models/employee';
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'employee-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employee-table.component.html',
   styleUrl: './employee-table.component.css'
 })
 export class EmployeeTableComponent {
 
   employees: Employee[] = [];
-  
+
   constructor(private employeeService: EmployeeService) {}
 
   ngOnInit(){
