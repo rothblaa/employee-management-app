4fc105112c8a0dceb15415661095bea2a5b8400c
rothblaa <adam.j.rothblatt@outlook.com>
2024-10-09 10:53:18 -0500

    creating employee form and data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { EmployeeTableComponent } from './employee-table/employee-table.component';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EmployeeTableComponent],
+  imports: [RouterOutlet, EmployeeTableComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
